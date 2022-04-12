4884741fb73450483c75ac0bdbb677c38cfe4427
chenjinhui0738 <1228793447@qq.com>
2022-04-12 11:23:13 +0800

    guava

---
+++ guava/src/test/java/com/cjh/guava/GuavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjh.guava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
