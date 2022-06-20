71741f9974eb7a3b20602727cb103ef47534db29
chenjinhui0738 <1228793447@qq.com>
2022-06-20 15:53:19 +0800

    canal基本使用

---
+++ canal/src/test/java/com/cjh/canal/CanalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjh.canal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
