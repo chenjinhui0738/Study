12c7a16ea9537a0936328f213b743d09e22d4122
chenjinhui0738 <1228793447@qq.com>
2022-07-22 11:20:34 +0800

    jsoup防御xss攻击

---
+++ jsoup/src/main/java/com/cjh/jsoup/JsoupApplication.java
@@ -0,0 +1,13 @@
+package com.cjh.jsoup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsoupApplication.class, args);
+    }
+
+}
